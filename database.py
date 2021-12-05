6b2445ad58d5a9ca6efeec26c8e34862ba99bc51
Максим Семенов <maxim-semjonov@yandex.ru>
2021-12-05 13:59:55 +0300

    some shitcoding

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import sqlite3
 import redis
 from datetime import datetime
 
+
 # структура базы данных:
 # user_id id юзера
 # user_name имя
@@ -13,7 +14,7 @@ from datetime import datetime
 # user_data  должна содержать user_id, user_name, user_surname, username
 
 
-def add_user(user_data):
+def add_user_to_db(user_data):
     """
     это функция для работы с базой данных
     :param user_data:
@@ -24,16 +25,14 @@ def add_user(user_data):
     user_history = ''
     connection = sqlite3.connect('bot.db', check_same_thread=False)
     cursor = connection.cursor()
-    cursor.execute("SELECT user_id FROM clients WHERE user_id = {user_id}". format(user_id=user_id))
+    cursor.execute("SELECT user_id FROM clients WHERE user_id = {user_id}".format(user_id=user_id))
     responce = cursor.fetchone()
-    print(responce)
     if responce is None:
-        cursor.execute("INSERT INTO clients VALUES(?,?,?,?,?,?);", (
+        cursor.execute("INSERT INTO clients VALUES(?,?,?,?,?);", (
             user_id,
             user_name,
             user_surname,
             username,
-            user_status,
             user_history)
                        )
         connection.commit()
@@ -41,28 +40,19 @@ def add_user(user_data):
     else:
         cursor.execute("SELECT * FROM clients WHERE user_id = {user_id}".format(user_id=user_id))
         responce = cursor.fetchone()
-        print(responce)
         return 'пользователь существует'
-
-
-
-
-
-# redis = redis.Redis()
-# redis.mset({'croatia': 'zagreb', 'russia': 'vyborg', 'germany': 'berlin'})
-# print(redis.get('russia'))
-# print(redis.get('france'))
-
-
-
 
 
 class User:
     """
-    в классе пользователь будут хранится данные пользователя во время сессии
+    в классе пользователь будут хранится данные пользователя во время сессии.
+    когда пользователь запускает диалог, запускается классметод get_user.
+    Если пользователь есть в словаре
     """
-    # users = dict()
-    redis = redis.Redis()
+    users = dict()
+
+    # tusers = {1234: '244', 35341: 'dsfgsg', 251232: 'dsfs'}
+    # redis = redis.Redis()
 
     def __init__(self, user_data):
         self.id = user_data[0]
@@ -71,24 +61,144 @@ class User:
         self.user_req = []
         self.req_data = datetime.now()
         self.is_running = False
+        # self.lang = 'eng'
+        # self.step = dict()
         User.add_user(user_data[0], self)
 
     @classmethod
     def add_user(cls, id, user):
-        cls.redis.mset({str(id): user})
-
+        cls.users[id] = user
+        print(" сработал адд юзер из классметода")
 
     @classmethod
     def get_user(cls, user_data):
-        if cls.redis.get(user_data[0]) is None:
-            User(user_data)
-        return cls.redis.get(user_data[0])
+        id = user_data[0]
+        if id in cls.users.keys():
+            print('сработал гетюзер. пользователь найден')
+            user_obj = cls.users[id]
+        else:
+            print('сработал гетюзер. не найден')
+            user_data_from_db = test_get_user_from_db(id)  # запрос на наличие пользователя в бж
+            print(user_data_from_db)
+            if not user_data_from_db:  # если вернулся None
+                test_add_new_user_to_db(user_data)  # добавляем в базу
+            user_obj = User(user_data_from_db)  # иначе обновляем запись о пользователе
+        print(user_obj)
+        return user_obj
+
+
+def work_with_keys(user_data, key):
+    client = User.get_user(user_data)
+    client.user_req.append(key)
+
+
+def return_all_req(user_data):
+    client = User.get_user(user_data)
+    return client.user_req
+
+
+def test_get_user_from_db(user_id):
+    """
+    это функция для работы с базой данных
+    :param user_data:
+    :return:
+    """
+    connection = sqlite3.connect('bot.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute("SELECT user_id FROM clients WHERE user_id = {user_id}".format(user_id=user_id))
+    responce = cursor.fetchone()
+    print('сработал гетюзер из дб его ответ:', responce)
+    if responce is None:
+        return False
+    else:
+        cursor.execute("SELECT * FROM clients WHERE user_id = {user_id}".format(user_id=user_id))
+        responce = cursor.fetchone()
+        print('юзер нашёлся в дб.его ответ:', responce)
+        return responce
+
+
+def test_add_new_user_to_db(user_data):
+    """
+    это функция для работы с базой данных
+    :param user_data:
+    :return:
+    """
+    user_id, user_name, user_surname, username = user_data
+    user_history = ''
+    connection = sqlite3.connect('bot.db', check_same_thread=False)
+    cursor = connection.cursor()
+    cursor.execute("SELECT user_id FROM clients WHERE user_id = {user_id}".format(user_id=user_id))
+    responce = cursor.fetchone()
+    if responce is None:
+        cursor.execute("INSERT INTO clients VALUES(?,?,?,?,?);", (
+            user_id,
+            user_name,
+            user_surname,
+            username,
+            user_history)
+                       )
+        connection.commit()
+        return True
+    else:
+        raise Exception('ПОЛЬЗОВАТЕЛЬ УЖЕ ЕСТЬ!!')
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# d = {1234: '244', 35341: 'dsfgsg', 251232: 'dsfs'}
+# a = 1234
+# b = a in d.keys()
+# print(b)
+# if a in d.keys():
+#     print('da')
+# else:
+#     print('net')
+
+
+# tuser_data = [1234, 'asdg','adgasd','adgasd']
+# a = User.get_user(tuser_data)
+# print(a)
 ######################################################################################
+    #
+    #
+    # @classmethod
+    # def test_get_user(cls, user_data):
+    #     id = user_data[0] # кладем id в переменную
+    #     test_user_obj = cls.redis.get(id) #запрос в редис на наличие присутствие записи о польлзователе
+    #     if cls.redis.get(id) is None: #если нет в редисе
+    #         user_data_from_db = test_get_user_from_db(id) # запрос на наличие пользователя в бж
+    #         if not user_data_from_db:# если вернулся None
+    #             test_add_new_user_to_db(user_data) #добавляем в базу
+    #         test_user_obj = User(user_data_from_db)# иначе обновляем запись о пользователе
+    #     return test_user_obj #возвращаем объект
+
+
+
 # class User:
 #     """
 #     вместо базы данных пока что буду пользоваться классом. Ровно до того момента как будут реализованы функции.
@@ -151,3 +261,16 @@ class User:
 #         self.cursor.execute(request, (value, user_id))
 #         self.connect.commit()
 #         self.close_connect()
+
+
+# @classmethod
+#     def get_user(cls, user_data):
+#         id = user_data[0]
+#         if id in cls.users.keys():
+#             print('пользователь найден')
+#             user_obj = cls.users[id]
+#         else:
+#             print('не найден')
+#             user_obj = User(user_data)
+#
+#         return user_obj
--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import sqlite3
 import functools
+import os
 import keyboard as kb
 from telebot import TeleBot
-from settings import commands, buttons_eng, API_TOKEN, User, content_type_ANY
-from database import add_user
+from settings import commands, buttons_eng, API_TOKEN, content_type_ANY
+from database import add_user_to_db, User, work_with_keys, return_all_req
 
 bot = TeleBot(API_TOKEN)
-
-
-
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     """
-    обработчик команды старт
+    обработчик команды старт.
+    1. собираю в переменную данные о пользователе.
+    на его основе создаю экземпляр класса User, ставлю флаг активной сессии.
+    формирую приветствие и отправляю сообщение. сразу вызываю main_choice
     :param message:
     :return:
     """
-    client = User.get_user(message.from_user.id, message.from_user.first_name, message.from_user.last_name)
+    user_data = [message.from_user.id,
+                 message.from_user.first_name,
+                 message.from_user.last_name,
+                 message.from_user.username]
+    print('юзер дата из start', user_data)
+    client = User.get_user(user_data)  # создаем экземпляр для работы
+    print(client)
+    client.is_running = True  # ставим метку активной сессии
     chat_id = message.chat.id
-    db_response = add_user((message.from_user.id, message.from_user.first_name, message.from_user.last_name, message.from_user.username))
-    text = f"Hi! {client.name} {client.surname}!\n {buttons_eng['greet']} {client.id}\n{db_response})"
+    text = f"Hi! {client.name} {client.surname}!\n {buttons_eng['greet']} {client.id}\n)"
     bot.send_message(chat_id, text)
     main_choice(message)
 
 
-@bot.message_handler(commands=['help'])# хелп
+@bot.message_handler(commands=['help'])  # хелп
 def help_message(message):
     """
     Обработчик команды хелп
@@ -43,7 +50,8 @@ def help_message(message):
 @bot.message_handler(commands=['main'])
 def main_choice(message):
     """
-    главное меню с кнопками. клавиатура и кнопки берутся из файла keyboard
+    главное меню с кнопками. клавиатура и кнопки берутся из файла keyboard.
+    отправляю сообщение с инлайн кнопками
     :param message:
     :return:
     """
@@ -51,10 +59,10 @@ def main_choice(message):
     bot.send_message(message.from_user.id, text_message, reply_markup=kb.main_kb)
 
 
-@bot.message_handler(content_types=['text']) #ответчик на текст
+@bot.message_handler(content_types=['text'])  # ответчик на текст
 def text_handler(message):
     """
-    Ответчик на текстовое сообщение
+    Ответчик на текстовое сообщение, добавлено в тестовых целях
     :param message:
     :return:
     """
@@ -75,7 +83,7 @@ def text_handler(message):
         bot.send_message(chat_id, "i don't understand")
 
 
-@bot.message_handler(content_types=[content_type_ANY]) #ответчик на ANY
+@bot.message_handler(content_types=[content_type_ANY])  # ответчик на ANY
 def text_handler(message):
     """
     Ответчик если пользователь послал: документ, войс, пикчу, видео, аудио.
@@ -86,53 +94,83 @@ def text_handler(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, 'incorrect data')
 
+
 #################################################
 # ответчики
 
 
-@bot.callback_query_handler(func=lambda call: call.data in ['low', 'high']) #ветка лоу прайс
+@bot.callback_query_handler(func=lambda call: call.data in ['low', 'high'])  # ветка прайс лоу и хай
 def callback_main_menu(call):
     """
-    Ответчик на пункт меню low
+    Ответчик на пункт меню low и high.
+    снова создаю переменную с юзер дата для того что бы с помощью
+    функции work_with_keys записываю в экземпляр то что выбрал пользователь, что бы
+    формировать его историю запроса.
+    (метод наиболее дегенеративный, переписать!)
+    в переменную текст формирую ответ, отправляю пользователю, исполняю следующий шаг
     :param call:
     :return:
     """
+    user_data = [call.from_user.id,
+                 call.from_user.first_name,
+                 call.from_user.last_name,
+                 call.from_user.username]
     key = call.data
     chat_id = call.message.chat.id
     text = f"Exellent! Your choice is {key} price hotel!\nNow, send me country name"
-    # text2 = call.message
+    work_with_keys(user_data, key)
     bot.send_message(chat_id, text)
-    bot.register_next_step_handler(call.message, low_price_cntr)
+    bot.register_next_step_handler(call.message, choise_city)
 
 
-def low_price_cntr(message):
+def choise_city(message):
+    """
+    снова создаю переменню с данными пользователя что бы добавить в список
+    историю запроса
+    выполняется простенькая проверка на наличие недопустимых символов(цифр) в тексте.
+    как вариант переделать на регулярку, что бы в тексте были только буквы без символов
+    с помощью функции  work_with_keys записываю то что ввел пользователь
+
+    :param message:
+    :return:
+    """
+    user_data = [message.from_user.id,
+                 message.from_user.first_name,
+                 message.from_user.last_name,
+                 message.from_user.username]
+
     chat_id = message.chat.id
     text = message.text
     if not text.isalpha():
         message = bot.send_message(chat_id, 'country name has incorrect symbols')
-        bot.register_next_step_handler(message, low_price_cntr) #askSource
+        bot.register_next_step_handler(message, choise_city)  # askSource
         return
+    work_with_keys(user_data, text)
     message = bot.send_message(chat_id, 'your input is ' + text + ' Now, enter the city')
     bot.register_next_step_handler(message, low_price_city)
 
+
 def low_price_city(message):
+    """
+    почти всё тоже самое что и в предыдущем шаге, только с помощью функции return_all_req
+    смотрю что ввел пользователь.
+    :param message:
+    :return:
+    """
+    user_data = [message.from_user.id,
+                 message.from_user.first_name,
+                 message.from_user.last_name,
+                 message.from_user.username]
+
     chat_id = message.chat.id
     text = message.text
     if not text.isalpha():
         message = bot.send_message(chat_id, 'city name has incorrect symbols')
         bot.register_next_step_handler(message, low_price_city)
         return
-    bot.send_message(chat_id, 'here is some result from api. FIN')
-
-
-
-
-
-
-
-
-
-
+    work_with_keys(user_data, text)
+    rspomce = return_all_req(user_data)
+    bot.send_message(chat_id, f'here is some result {rspomce}. FIN')
 
 
 bot.infinity_polling()
